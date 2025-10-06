7eca66f3a54edd1f393d69293d04af11995d794e
gilli-jonathan <gilli.jonathan97@gmail.com>
2025-10-06 14:00:05 +0200

    create first task about name and ages

---
+++ script.js
@@ -0,0 +1,11 @@
+// Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+
+/*MACRO TASK N.1
+    //chiedere l'età 
+    //chiedere i kilometri
+
+    TOOLS:
+    - Variabili 
+    - prompt 
+*/
+
